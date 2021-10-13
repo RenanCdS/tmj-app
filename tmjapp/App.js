85db14efb2bdbb84a0a1a5f51c72f747529a21cb
RenanCdS <renan.cds0911@gmail.com>
2021-10-12 21:56:37 -0300

    Initial commit

---
+++ tmjapp/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import IntroPage from './pages/IntroPage';
+
+const App: () => Node = () => {
+
+  return (
+   <>
+    <IntroPage />
+   </>
+  );
+};
+
+export default App;
---
+++ tmjapp/pages/IntroPage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+
+const IntroPage = () => {
+
+    return (
+        <View>
+            <ImageBackground 
+                source={require('../images/intro-page/intro-image.jpg')}
+                resizeMode='cover'
+                style={styles.image}
+            >
+                <LinearGradient colors={['rgba(20, 130, 64, 0.8)', 'rgba(46, 227, 119, .4)']} style={styles.linearGradient}>
+                    <View>
+                        <Text style={styles.maintitle}>TMJ</Text>
+                    </View>
+                    <View style={styles.container}>
+                        <Button style={styles.button} mode='outlined' onPress={() => console.log('Pressed')}>Registrar</Button>
+                        <Button style={styles.button} mode='contained'>Entrar</Button>
+                    </View>
+                </LinearGradient>
+            </ImageBackground>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        display: 'flex',
+        marginTop: 30,
+        justifyContent: 'center'
+    },  
+    image: {
+        height: '100%',
+        width: '100%'
+    },
+    linearGradient: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        width: '80%',
+        padding: 10,
+        margin: 10,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+    },
+    maintitle: {
+        fontSize: 64,
+        color: '#fff',
+        fontWeight: '600',
+        opacity: 1
+    }
+})
+
+export default IntroPage;
